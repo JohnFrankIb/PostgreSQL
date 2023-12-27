f9dbce00a29b2bf9a8da302b02feed9509c91628
JohnFrank <Frank_Ibarra400@hotmail.com>
2023-12-27 16:43:07 -0600

    Primer commit

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-#POSTGRESQL
 
-Diana = "Amor de mi vida"
-Frank = "Ama a Diana."
+import psycopg2
+
+
+# Conexión con la base de datos PostgreSQL
+try:
+    conn = psycopg2.connect(
+        host = "localhost",
+        user = "postgres",
+        password = "scared",
+        database = "prueba"
+    )
+
+    print("Conexión exitosa.")
+
+except Exception as ex:
+    print(ex)
+finally:
+    conn.close()
+    print("Conexión finalizada.")
