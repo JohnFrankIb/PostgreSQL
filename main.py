e7f6097bd79e7afd07daa1b8b2f10f239dbef8b0
JohnFrank <Frank_Ibarra400@hotmail.com>
2023-12-29 15:20:19 -0600

    create table

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 import psycopg2
 import pandas as pd
-
+from sqlalchemy import create_engine
 
 # Conexión con la base de datos PostgreSQL
 conn = psycopg2.connect(
-    host = "localhost",
     user = "postgres",
     password = "scared",
+    host = "localhost",
     database = "prueba"
 )
 
-print("Conexión exitosa.")
+print("Conexión realizada.")
+
 
 # Definimos el cursor
 cur = conn.cursor()
 
 # Consulta para ejecutar
-cur.execute("""CREATE TABLE IF NOT EXISTS person (
+cur.execute(
+    """CREATE TABLE IF NOT EXISTS person (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30),
-    age INT,
-    gender CHAR
+    name VARCHAR(30) NOT NULL,
+    age INT NOT NULL,
+    gender CHAR NOT NULL
     )""")
 
 
-cur.execute("""INSERT INTO person (name, age, gender) VALUES
-            (1, "Mike", 30, "m"),
-            (2, "Lisa", 30, "f"),
-            (3, "John", 54, "m"),
-            (4, "Bob", 80, "m"),
-            (5, "Julie", 40, "f")
-            """)
+cur.execute(
+    """INSERT INTO person(name, age, gender)
+    VALUES ('Frank', 24, 'm'),
+    ('Diana', 22, 'f'),
+    ('Roberto', 56, 'm'),
+    ('Juan', 54, 'm'),
+    ('Iker', 45, 'm'),
+    ('Pepe', 74, 'm'),
+    ('Diego', 62, 'm'),
+    ('Roberto', 21, 'm'),
+    ('Jacobo', 18, 'm')
+    """)
 
 # Enviamos las consultas
 conn.commit()
@@ -41,3 +48,16 @@ print("Consultas ejecutada.")
 cur.close()
 conn.close()
 print("Conexión finalizada.")
+
+# ------------------------------------------------------------------------
+
+# Creación de engine para visualizar base de datos
+engine = create_engine('postgresql+psycopg2://postgres:scared@localhost:5432/prueba')
+
+dbconn = engine.connect()
+
+# Utilizamos Pandas para mostrar los datos
+#pd.options.display.max_rows <-- configuración para mostrar todos los datos 
+
+df = pd.read_sql_table("person", dbconn, index_col='id')
+print(df)
