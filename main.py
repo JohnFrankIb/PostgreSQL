b4de248bbb35abccfcec8a4f0ca4e12565325f71
JohnFrank <Frank_Ibarra400@hotmail.com>
2023-12-30 03:31:32 -0600

    update

--- main.py
+++ main.py
@@ -10,54 +10,74 @@ conn = psycopg2.connect(
     database = "prueba"
 )
 
-print("Conexión realizada.")
-
 
 # Definimos el cursor
 cur = conn.cursor()
 
-# Consulta para ejecutar
-cur.execute(
-    """CREATE TABLE IF NOT EXISTS person (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    age INT NOT NULL,
-    gender CHAR NOT NULL
-    )""")
+# Consultas para ejecutar
 
+def new_table():
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS person (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(30) NOT NULL,
+        age INT NOT NULL,
+        gender CHAR NOT NULL
+        )""")
+    conn.commit()
+    print("Tabla creada.")
+    cur.close()
+    conn.close
+    print("Conexión finalizada.")
 
-cur.execute(
-    """INSERT INTO person(name, age, gender)
-    VALUES ('Frank', 24, 'm'),
-    ('Diana', 22, 'f'),
-    ('Roberto', 56, 'm'),
-    ('Juan', 54, 'm'),
-    ('Iker', 45, 'm'),
-    ('Pepe', 74, 'm'),
-    ('Diego', 62, 'm'),
-    ('Roberto', 21, 'm'),
-    ('Jacobo', 18, 'm')
-    """)
+def query():
+    cur.execute(
+        """INSERT INTO person(name, age, gender)
+        VALUES ('Frank', 24, 'm'),
+        ('Diana', 22, 'f'),
+        ('Roberto', 56, 'm'),
+        ('Juan', 54, 'm'),
+        ('Iker', 45, 'm'),
+        ('Pepe', 74, 'm'),
+        ('Diego', 62, 'm'),
+        ('Roberto', 21, 'm'),
+        ('Jacobo', 18, 'm')
+        """)
+    conn.commit()
+    print("Consulta ejecutada")
+    cur.close()
+    conn.close()
+    print("Conexión finalizada.")
 
-# Enviamos las consultas
-conn.commit()
-print("Consultas ejecutada.")
+def new_query():
+    cur.execute(
+        """SELECT DISTINCT name
+        FROM person;
+        """)
+    cur.close()
+    conn.close()
+    print("Conexión finalizada.")
 
-
-# Cerramos el cursor y la conexión.
+query = """SELECT DISTINCT name
+    FROM person;
+    """
 cur.close()
 conn.close()
-print("Conexión finalizada.")
 
 # ------------------------------------------------------------------------
 
-# Creación de engine para visualizar base de datos
+#   Creación de engine para visualizar base de datos con SQLAlchemy
 engine = create_engine('postgresql+psycopg2://postgres:scared@localhost:5432/prueba')
+enconn = engine.connect()
 
-dbconn = engine.connect()
-
-# Utilizamos Pandas para mostrar los datos
+#   Utilizamos Pandas para mostrar los datos
 #pd.options.display.max_rows <-- configuración para mostrar todos los datos 
 
-df = pd.read_sql_table("person", dbconn, index_col='id')
+df = pd.read_sql_query(query, enconn)
 print(df)
+
+enconn.close
+
+
+# ARREGLAR LAS FUNCIONES (SQLALCHEMY NO PUEDE TRABAJAR CON FUNCIONES)
+# ASIGNAR LAS FUNCIONES A VARIABLES
